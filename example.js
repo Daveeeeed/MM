635319ef9d4179e752e4bef3265458cfa4b3e4f9
Daveeeeed <dati981026@gmail.com>
2020-05-19 15:39:20 +0200

    Bozza di parser

--- example.js
+++ example.js
@@ -1,21 +1,29 @@
-var page_index = 0;
+var page = 0;
 
 $(document).ready(function () {
-
-    $('#right_arrow').click(function () {
-        var text = $('#textbox');
-        page_index++;
-        setCorrectText(text);
-    });
-    $('#left_arrow').click(function () {
-        var text = $('#textbox');
-        page_index--;
-        setCorrectText(text);
+    $.getJSON('example.json', function (data) {
+        var main = $("body");
+        let string = "";
+        string = string.concat(mergeChild(data.gui));
+        main.html(string);
     });
 });
 
-function setCorrectText(text) {
-    $.getJSON('example.json', function (data) {
-        text.text(data.items[page_index].name);
-    });
+function parametersMerge(array) {
+    let s = "";
+    for (let i = 0; i < array.length; i++){
+        s = s.concat(' ',array[i].key,'= "',array[i].value,'"');
+    }
+    return s;
+}
+
+function mergeChild(children) {
+    let s = "";
+    for (let i = 0; i < children.length; i++){
+        s = s.concat("<", children[i].tag, parametersMerge(children[i].parameters), ">");
+        s = s.concat(children[i].content);
+        s = s.concat(mergeChild(children[i].children));
+        s = s.concat("</",children[i].tag,">");
+    }
+    return s;
 }
