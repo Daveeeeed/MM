4b04875596aa56deb59042f1f54de8a8e0059120
Daveeeeed <dati981026@gmail.com>
2020-12-31 14:55:55 +0100

    2

--- user.js
+++ user.js
@@ -9,7 +9,7 @@ router.get("/testing", function (req, res, next) {
 
 /* GET homepage */
 router.get("/", function (req, res, next) {
-  res.sendFile("./views/homepage.html");
+  res.sendFile(path.resolve("views/homepage.html"));
 });
 
 /* GET editor */
