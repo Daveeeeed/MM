0a3b027ac47ae94867115a547cbe8e193e40c6d9
Daveeeeed <dati981026@gmail.com>
2020-12-31 01:21:59 +0100

    a

--- bin/www.js
+++ bin/www.js
@@ -3,7 +3,7 @@
 const app = require("../app");
 const http = require("http");
 
-const port = "10000";
+const port = "8000";
 
 app.listen(port, function () {
     console.log("Server running on port 8000!");
