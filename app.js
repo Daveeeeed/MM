ca38c4233b9df35f752498908a4da265f156bcda
Daveeeeed <dati981026@gmail.com>
2021-01-02 15:26:37 +0100

    25

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ app.use(express.json());
 app.use("/public", express.static("/webapp/MM/public"));
 
 app.use((req, res, next) => {
-  if (!req.secure) {
-      res.redirect("https://site181982.tw.cs.unibo.it" + req.originalUrl)
+  console.log(req.protocol);
+  console.log(re.secure);
+  if (req.protocol == "http") {
+    res.redirect("https://site181982.tw.cs.unibo.it" + req.originalUrl);
   } else next();
 });
 
