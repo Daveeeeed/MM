e983ecdd488b9cbb4fb318de549764636bd69318
Daveeeeed <dati981026@gmail.com>
2021-01-02 14:54:40 +0100

    22

--- bin/www.js
+++ bin/www.js
@@ -5,6 +5,6 @@ const https = require("https");
 
 const port = "8000";
 
-app.listen(port, function () {
-    console.log("Server running on port 8000!");
+https.createServer(app).listen(port, function () {
+  console.log("Server running on port 8000!");
 });
