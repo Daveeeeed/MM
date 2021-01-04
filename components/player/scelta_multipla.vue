ef11fa75d2d744821fe25368099ddd3548c9f5de
matte2712 <matteo.feroli@gmail.com>
2021-01-04 17:45:20 +0100

    95

--- components/player/scelta_multipla.vue
+++ components/player/scelta_multipla.vue
@@ -3,7 +3,7 @@
     <div class="activity-text" id="question">
       {{ element.component.question }}
     </div>
-    <b-form-group>
+    <b-form-group role="group">
       <b-form-radio
         v-for="(radio_answer, index) in element.component.answers"
         :key="index"
