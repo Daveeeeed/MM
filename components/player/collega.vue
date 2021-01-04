7f28674336a419cbb2662ffcdd9db5e21fb7a844
Daveeeeed <dati981026@gmail.com>
2021-01-04 14:36:32 +0100

    79

--- components/player/collega.vue
+++ components/player/collega.vue
@@ -1,23 +1,63 @@
 <template>
-  <div></div>
+  <div>
+    <h2>Prime parti</h2>
+    <div v-for="(part, index) in first_parts" :key="index">
+      {{part.phrase}}
+    </div>
+    <h2>Seconde parti</h2>
+    <div v-for="(part, index) in second_parts" :key="index">
+      {{part.phrase}}
+    </div>
+  </div>
 </template>
 
 <script>
 module.exports = {
   data() {
-    return {};
+    return {
+      first_parts: [],
+      second_parts: [],
+    };
   },
   props: {
     element: Object,
+    answer_confirmed: Boolean,
+  },
+  computed: {
+    is_answer_done: function () {
+      return false;
+    },
   },
   methods: {
-    sendAnswer(){
-      let answer;
-      this.$emit("answer-sent", answer);
-    }
-  }
+    sendAnswer() {
+      let is_correct = false;
+      this.$emit("answer-sent", is_correct);
+    },
+  },
+  watch: {
+    answer_confirmed(isConfirmed) {
+      if (isConfirmed) {
+        if (this.is_answer_done) this.sendAnswer();
+        this.$emit("answer-checked");
+      }
+    },
+  },
+  mounted: function () {
+    // Array setup
+    this.element.component.answers.forEach((answer, index) => {
+      this.first_parts.push({
+        index: index,
+        phrase: answer.first,
+      });
+      this.second_parts.push({
+        index: index,
+        phrase: answer.second,
+      });
+    });
+    this.first_parts.sort(() => 0.5 - Math.random());
+    this.second_parts.sort(() => 0.5 - Math.random());
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
