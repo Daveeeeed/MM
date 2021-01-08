e2cbdf65734e3217231a66563e06f75368973f7e
Daveeeeed <dati981026@gmail.com>
2021-01-08 19:56:13 +0100

    111

--- components/player/immagine.vue
+++ components/player/immagine.vue
@@ -1,5 +1,14 @@
 <template>
-  <img class="activity-image" :src="element.component.url" :alt="element.component.description" />
+  <div class="d-flex flex-column align-items-center" style="width: 100%">
+    <img
+      class="activity-image"
+      :src="element.component.url"
+      :alt="element.component.description"
+    />
+    <div class="activity-text" style="text-align: center">
+      {{ element.component.description }}
+    </div>
+  </div>
 </template>
 
 <script>
